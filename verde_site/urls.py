5afe390686161daaf4bc82951760371a6ab2b705
Cristobal Aguirre <cristobal.aguirre89@gmail.com>
2019-03-03 10:32:48 -0800

    switched auth to JWT

--- verde_site/urls.py
+++ verde_site/urls.py
@@ -16,14 +16,19 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls import include
+
 from rest_framework.schemas import get_schema_view
 from rest_framework.documentation import include_docs_urls
+from rest_framework_simplejwt import views as jwt_views
 
-schema_view = get_schema_view(title='Pastebin API')
+
+schema_view = get_schema_view(title='Verde Sazon API')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api-auth/', include('rest_framework.urls')),
+    path('api/token/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
+    #path('api-auth/', include('rest_framework.urls')),
     path('api/blog/', include('blog.urls')),
     path('api/restaurant/', include('restaurant.urls')),
     path('api/users/', include('users.urls')),
